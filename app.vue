97843c5ec5b8c8529b551daa1f11f67159a722a5
Rey Garcia <jreygarcia24@gmail.com>
2023-04-13 22:15:08 +0800

    UI component for nft card

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="main">
+    <NFTPreviewCard />
+
+    <footer class="attribution">
+      <div>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a></div>
+      <div>Coded by <a href="https://github.com/brandeth" target="_blank">Rey Garcia</a></div>
+    </footer>
+  </div>
+</template>
---
+++ components/NFTPreviewCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="NFTPreviewCard">
+    <div class="NFTPreviewCard-imageContainer">
+      <img class="NFTPreviewCard-image" src="@/assets/images/image-equilibrium.jpg" alt="Equilibrium">
+      <div class="NFTPreviewCard-preview">
+        <img src="@/assets/images/icon-view.svg" alt="Icon-view">
+      </div>
+    </div>
+    <h1 class="NFTPreviewCard-title">
+      <span>Equilibrium #3429</span>
+    </h1>
+    <p class="NFTPreviewCard-description">Our Equilibrium collection promotes <br /> balance and calm.</p>
+    <div class="NFTPreviewCard-detailsContainer">
+      <div class="NFTPreviewCard-currentValue">
+        <img src="@/assets/images/icon-ethereum.svg" alt="Ethereum">
+        <span>0.04 ETH</span>
+      </div>
+      <div class="NFTPreviewCard-daysRemaining">
+        <img src="@/assets/images/icon-clock.svg" alt="Clock">
+        <span>3 days left</span>
+      </div>
+    </div>
+    <hr class="NFTPreviewCard-lineBreak" />
+    <footer class="NFTPreviewCard-footer">
+      <div class="NFTPreviewCard-avatar">
+        <img src="@/assets/images/image-avatar.png" alt="Avatar">
+      </div>
+      <p class="NFTPreviewCard-creatorContainer">
+        <span>Creation of </span> <span class="NFTPreviewCard-creator">Jules Wyvern</span>
+      </p>
+    </footer>
+  </div>
+</template>
